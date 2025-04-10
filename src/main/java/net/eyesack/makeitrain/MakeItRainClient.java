c7ae88cbb27b116e58d09de22da870e160bfd2c0
EyeSackSea <justdoinstuffforstuff@gmail.com>
2025-04-09 23:31:17 -0400

    first commit

---
+++ src/main/java/net/eyesack/makeitrain/MakeItRainClient.java
@@ -0,0 +1,11 @@
+package net.eyesack.makeitrain;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MakeItRainClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient(){
+        
+    }
+
+}
